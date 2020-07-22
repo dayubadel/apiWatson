d419416d57c2ab9d9500f4b890e0377dc2bcefaf
Bryan Garcia <bryan.garcia@gaiaconsultores.biz>
2020-07-22 13:03:45 -0500

    actualizacion modulo  registro productos - pruebas unitarias

--- models/productoModel.js
+++ models/productoModel.js
@@ -1,27 +1,30 @@
 
 class Producto {
     constructor(idVitex, nombre, idRefSAP, stockCC, stockOtroPago, descuentoCC, descuentoOtroPago,
-        precioCC, precioOtroPago, interesCC, interesOtroPago, activo, visible, fechaCreacionWS, fechaModificaWS,
-        imagenes, marca, caracteristicas, categorias){
+        precioCC, precioOtroPago, cuotasPrecioCC, plazoGarantia, activo, visible, fechaCreacionWS, fechaModificaWS,
+        imagenes, marca, caracteristicas, categorias, isMarketPlace, url){
             this.idVitex = idVitex || "vacio";
             this.nombre = nombre || "vacio";
             this.idRefSAP = idRefSAP || "vacio";
             this.stockCC = stockCC || 0;
             this.stockOtroPago = stockOtroPago || 0;
-            this.descuentoCC = descuentoCC || 0;
-            this.descuentoOtroPago = descuentoOtroPago || 0;
+            // this.descuentoCC = descuentoCC || 0;
+            // this.descuentoOtroPago = descuentoOtroPago || 0;
             this.precioCC = precioCC || 0;
             this.precioOtroPago = precioOtroPago || 0;
-            this.interesCC = interesCC || 0;
-            this.interesOtroPago = interesOtroPago || 0;
-            this.activo = activo || 0;
-            this.visible = visible || 0;
-            this.fechaCreacionWS = fechaCreacionWS || null;
-            this.fechaModificaWS = fechaModificaWS || null;
+            this.cuotasPrecioCC = cuotasPrecioCC || 0;
+            this.plazoGarantia = plazoGarantia || 0;
+
+            // this.activo = activo || 0;
+            // this.visible = visible || 0;
+            // this.fechaCreacionWS = fechaCreacionWS || null;
+            // this.fechaModificaWS = fechaModificaWS || null;
             this.imagenes = imagenes || [];
             this.marca = marca || {}
             this.caracteristicas = caracteristicas || {};
             this.categorias = categorias || [{}]
+            this.isMarketPlace = isMarketPlace || 0;
+            this.url = url || ''
         }
 }
 
